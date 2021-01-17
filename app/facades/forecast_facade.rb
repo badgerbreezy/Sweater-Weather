0b0637885fe4edff300c55722c0136679c2d3ffc
Brian Liu <brian.b.liu@gmail.com>
2021-01-17 15:47:46 -0700

    Edit forecastfacade to use forecast data instead of location for methods

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,41 +1,36 @@
 class ForecastFacade
   def self.get_forecast(location)
     results = WeatherService.get_weather_data(geocodes(location))
-    join_forecast(results)
-    # binding.pry
+    join_forecast(location)
   end
 
-  def self.join_forecast(results)
-    # binding.pry
-    forecast = {"current_weather": forecast_current(results), "daily_weather": forecast_daily(results), "hourly_weather": forecast_hourly(location)}
+  def self.join_forecast(location)
+    forecast = {"current_weather": forecast_current(location), "daily_weather": forecast_daily(location), "hourly_weather": forecast_hourly(location)}
   end
 
 
   def self.forecast_data(location)
     coordinates = geocodes(location)
     forecast = WeatherService.get_weather_data(coordinates)
-    binding.pry
   end
 
-  def self.forecast_current(results)
-    binding.pry
-    current = WeatherCurrent.new(results[:current])
+  def self.forecast_current(location)
+    WeatherCurrent.new(forecast_data(location)[:current])
   end
 
-  def self.forecast_daily(results)
-    results[:daily].map do |day|
+  def self.forecast_daily(location)
+    forecast_data(location)[:daily][0..4].map do |day|
       WeatherDaily.new(day)
     end
   end
 
-  def self.forecast_hourly(results)
-    results[:hourly].map do |day|
+  def self.forecast_hourly(location)
+    forecast_data(location)[:hourly][0..7].map do |day|
       WeatherHourly.new(day)
     end
   end
 
   def self.geocodes(location)
     GeocodeFacade.get_geolocation(location)
-    # binding.pry
   end
 end
