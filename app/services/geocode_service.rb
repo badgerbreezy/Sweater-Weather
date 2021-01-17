b7a03df44fbaed3d3af402985d4fd366da848ace
Brian Liu <brian.b.liu@gmail.com>
2021-01-17 02:06:41 -0700

    Create geocode and weather services

--- app/services/geocode_service.rb
+++ app/services/geocode_service.rb
@@ -1,12 +1,12 @@
 class GeocodeService
-  def get_location_data(location)
+  def self.get_location_data(location)
     response = conn.get("/geocoding/v1/address?key=#{ENV['MAPQUEST_API_KEY']}&location=#{location}")
     json = JSON.parse(response.body, symbolize_names: true)
   end
 
   private
 
-  def conn
+  def self.conn
     Faraday.new(url: 'http://www.mapquestapi.com')
   end
 end
--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,15 +1,16 @@
 class WeatherService
-  def get_weather_data(location)
+  def self.get_weather_data(location)
     response = conn.get("data/2.5/onecall") do |req|
       req.params[:lat] = location[:lat]
       req.params[:lon] = location[:lng]
+      # req.params['units'] = 'imperial'
     end
     json = JSON.parse(response.body, symbolize_names: true)
   end
 
   private
 
-  def conn
+  def self.conn
     Faraday.new(url: 'https://api.openweathermap.org/') do |req|
      req.params[:appid] = ENV['OPEN_WEATHER_API_KEY']
     end
