cc571523cd758a1a7f27b58b46261222ecf94593
Brian Liu <brian.b.liu@gmail.com>
2021-01-17 19:59:36 -0700

    Create image poro

---
+++ app/poros/image.rb
@@ -0,0 +1,16 @@
+class Image
+  attr_reader :id,
+              :image
+
+  def initialize(location, data)
+    @id = nil
+    @image = {
+      location:  location,
+      image_url: data[:contentUrl],
+      credit: {
+        source: data[:hostPageDomainFriendlyName]
+      }
+    }
+  end
+
+end
