10850258a3c968f6e3b44dd7b88ce08994b33dff
Brian Liu <brian.b.liu@gmail.com>
2021-01-20 00:35:18 -0700

    Refactor facades

--- app/facades/geocode_facade.rb
+++ app/facades/geocode_facade.rb
@@ -1,5 +1,5 @@
 class GeocodeFacade
   def self.get_geolocation(location)
-    location_data = MapquestService.get_location_data(location)[:results][0][:locations][0][:latLng]
+    MapquestService.get_location_data(location)[:results][0][:locations][0][:latLng]
   end
 end
